f9091c8f418671343548d052e439c6cd596f5e18
U-BSD\349973 <349973@SHS-10L74C3F1.beaverton.k12.or.us>
2018-10-25 09:05:02 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <videogames.h>
+#include <music.h>
+#include <movies.h>
+
+using namespace std;
+
+void getResponse(char response[10]) {
+  bool running = true;
+  cout << "Enter ADD, SEARCH, DELETE, or QUIT" << endl;
+  cin >> response;
+  running == true;
+}
+
+
+int main() {
+char response[10];
+ bool running = true;
+  getResponse(response);
+  while (running == true) {
+ if (response[0] == 'A'&& response[1] == 'D' && response[2] == 'D') {
+   cout << "Running add loop" << endl;
+   getResponse(response);
+ }
+ else if (response[0] == 'S' && response[1] == 'E' && response[2] == 'A' && response[3] == 'R' && response[4] == 'C' && response[5] == 'H') {
+   cout << "Running search loop" << endl;
+   getResponse(response);
+ }
+ else if (response[0] == 'D' && response[1] == 'E' && response[2] == 'L' && response[3] == 'E' && response[4] == 'T' && response[5] == 'E') {
+   cout << "Running delete loop" << endl;
+  getResponse(response);
+ }
+   else if (response[0] == 'Q' && response[1] == 'U' && response[2] == 'I' && response[3] == 'T') {
+     cout << "Quitting" << endl;
+     return 0;
+   }
+   else {
+     cout << "Enter a valid response" << endl;
+     getResponse(response);
+   }
+  }
+}
